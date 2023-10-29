5231bca37bd58ed0cc9f9cd2f04e6cfef3bc12d4
Andreea Burdui <andreea.burdui@gmail.com>
2023-10-29 19:16:46 +0200

    Created a loop for organizing

--- src/main/java/org/utm/labthree/AppLoop.java
+++ src/main/java/org/utm/labthree/AppLoop.java
@@ -62,7 +62,6 @@ public class AppLoop {
                     break;
             }}
             scanner.close();
-
         }}
 
 
--- src/main/java/org/utm/labthree/MainClass.java
+++ src/main/java/org/utm/labthree/MainClass.java
@@ -10,7 +10,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Date;
 import java.util.Scanner;
-
 public class MainClass {
 
     public static void main(String[] args) {
